ecf6767758476f5df3651030dd3ba5d987921589
ANKITA SAHOO <ankitacoder3@gmail.com>
2023-02-18 18:22:00 +0530

    Create Jenkinsfile.cpp

---
+++ main/Jenkinsfile.cpp
@@ -0,0 +1,33 @@
+pipeline {
+agent any
+stages {
+    stage('Build') {
+        steps {
+            sh 'g++ -o PES2UG20CS053-1 hello.cpp'
+        }
+    }
+    
+    stage('Test') {
+        steps {
+            sh './PES2UG20CS053-1'
+        }
+    }
+    
+    stage('Deploy') {
+        steps {
+            // deployment code
+            echo 'Deployment Successful!'
+        }
+    }
+}
+
+post {
+    always {
+        script {
+            if (currentBuild.result == "FAILURE") {
+                echo "Pipeline Failed..."
+            }
+        }
+    }
+}
+}
